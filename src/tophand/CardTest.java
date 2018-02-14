71c87ca10bfbbed9a4d2ac5f0b2e3b4b50a85d1a
g5fp <35341663+g5fp@users.noreply.github.com>
2018-02-14 09:55:30 -0600

    Update CardTest.java

--- src/tophand/CardTest.java
+++ src/tophand/CardTest.java
@@ -9,13 +9,14 @@ public class CardTest {
 		String suits = "";
 		Boolean result = false;
 		int[] handCounts = new int[10];
+		
 		// Scanner input = new Scanner(System.in);
 		// setting the number of hands dealt
-		// System.out.println("Enter the number of hands to deal:");
-		// int handsToDeal= input.nextInt();
-		// for (int j = 0; j < handsToDeal; j++) {
-		for (int j = 0; j < 1000000; j++) {
-			System.out.println("Hands Dealt:" + (j + 1));
+		//	System.out.println("Enter the number of hands to deal:");
+		//	int handsToDeal= input.nextInt();
+		//	for (int j = 0; j < handsToDeal; j++) {
+		for (int j = 0; j < 1000; j++) {
+			System.out.println("Hands Dealt:" + (j+1));
 			// create deck of cards from DeckOfCards class
 			DeckOfCards myDeckOfCards = new DeckOfCards();
 			for (int i = 0; i < 10; i++) {
@@ -25,83 +26,72 @@ public class CardTest {
 
 			suits = myHand.getSuits();
 			faces = myHand.getFaces();
-			// uncomment the two line below if you want to test a particular
-			// hand
+			// uncomment the two line below if you want to test a particular hand
 			// also set the loop to only deal 1 hand.
-			// faces = "2A543";
-			// suits = "SSHSS";
-
-			// sort the card high to low and low to high
+//			faces = "2A543";
+//			suits = "SSHSS";
+			
+			//sort the card high to low and low to high
 			String sortedCardsAceHigh = myHand.sortAceHigh(faces);
 			String sortedCardsAceLow = myHand.sortAceLow(faces);
-
-			// check hand rankings
+			
+			//check hand rankings
 			do {
-				result = myHand.isRoyalFlush(sortedCardsAceHigh, suits);
-				if (result) {
+				
+				if (result = myHand.isRoyalFlush(sortedCardsAceHigh,suits)) {
 					System.out.println("High Hand is: Royal Flush");
 					handCounts[0]++;
 					break;
 				}
-				result = myHand.isStraightFlush(sortedCardsAceHigh, suits);
-				if (result) {
+				if (result = myHand.isStraightFlush(sortedCardsAceHigh,suits)) {
 					System.out.println("High Hand is: Straight Flush");
 					handCounts[1]++;
 					break;
 				}
-				result = myHand.isQuads(faces);
-				if (result) {
+				if (result = myHand.isQuads(faces)) {
 					System.out.println("High Hand is: Quads");
 					handCounts[2]++;
 					break;
 				}
-				result = myHand.isFullHouse(faces);
-				if (result) {
+				if (result = myHand.isFullHouse(faces)) {
 					System.out.println("High Hand is: Full House");
 					handCounts[3]++;
 					break;
 				}
-				result = myHand.isFlush(suits);
-				if (result) {
+				if (result = myHand.isFlush(suits)) {
 					System.out.println("High Hand is: Flush");
 					handCounts[4]++;
 					break;
 				}
-				result = myHand.isStraight(sortedCardsAceHigh);
-				if (result) {
+				if (result = myHand.isStraight(sortedCardsAceHigh)) {
 					System.out.println("High hand is: Straight");
 					handCounts[5]++;
 					break;
 				}
-				result = myHand.isStraight(sortedCardsAceLow);
-				if (result) {
+				if (result = myHand.isStraight(sortedCardsAceLow)) {
 					System.out.println("High hand is: Straight");
 					handCounts[5]++;
 					break;
 				}
-				result = myHand.isTrips(faces);
-				if (result) {
+				if (result = myHand.isTrips(faces)) {
 					System.out.println("High Hand is: Trips");
 					handCounts[6]++;
 					break;
 				}
-				result = myHand.isTwoPair(faces);
-				if (result) {
+				if (result = myHand.isTwoPair(faces)) {
 					System.out.println("High Hand is: Two Pair");
 					handCounts[7]++;
 					break;
 				}
-				result = myHand.isPair(faces);
-				if (result) {
+				if (result = myHand.isPair(faces)) {
 					System.out.println("High Hand is: Pair");
 					handCounts[8]++;
 					break;
 				}
-
-				// if none of the above are true then the default is high hand
-				System.out.println("High Hand is: High Card");
-				handCounts[9]++;
-				break;
+					// if none of the above are true then the default is high hand
+					System.out.println("High Hand is: High Card");
+					handCounts[9]++;
+					break;
 			} while (result);
 
 			// printing the hand. Combining the suits and faces back together
@@ -110,8 +100,8 @@ public class CardTest {
 						+ suits.substring(i, i + 1) + " ");
 			}
 			System.out.println();
-		}
-
+		} // end of main for loop
+		
 		System.out.println("Hand Counts:");
 		System.out.println("Royal Flush:" + handCounts[0]);
 		System.out.println("Straight Flush:" + handCounts[1]);
